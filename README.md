b2413b286d6b7592e553ae1a3074d43e902777f4
necolo <negko311@gmail.com>
2022-02-03 16:49:11 +0800

    init project

--- README.md
+++ README.md
@@ -1,2 +1,108 @@
 # react-state
 A light react state manager based on React Hooks.
+
+You can use states across multiple react components. 
+
+## Usage
+It's very simple to use it in React hooks
+
+```typescript
+import { useMode, Provider } from '@necolo/react-state';
+
+// create your model
+function CounterModel () {
+  const [value, setValue] = useState(1);
+  function increase () {
+    setValue(value + 1);
+  }
+  return {
+    value,
+    increase,
+  };
+}
+
+// Registry model at the top component
+function App () {
+  return <Provider model={CounterModel}>
+    <SubPage />
+  </Provider>
+}
+
+// use model in sub component
+function SubPage () {
+  const counter = useModel(CounterModel);
+  return <div>
+    count: {counter.value}
+    <button onClick={counter.increase}>increase</button>
+  </div>;
+}
+```
+
+Models also accepts initial values
+
+```typescript
+function CounterModel(initValue) {
+  const [value, setValue] = useState(initValue);
+  return { value, setValue };
+}
+
+function App() {
+  // put initial value to args
+  return <Provider model={CounterModel} args={1}>
+    <SubPage />
+  </Provider>
+}
+
+function SubPage () {
+  const counter = useModel(CounterModel);
+  return <div>
+    count: {counter.value}
+    <button onClick={counter.increase}>increase</button>
+  </div>;
+```
+
+You can also use it in old react classes
+
+```typescript
+import { Consumer, withModel } from '@necolo/react-state';
+
+// use consumer in class to visit models 
+class SubClassPage extends React.Component {
+  public render () {
+    return <Consumer model={CounterStore}>
+      {(counter) => <div>{counter.value}</div>}
+    </Consumer>;
+  }
+}
+// or use HOC to wrap class and visit models from props
+const SubClassPage2 = withModel({counter: CounterStore}, class extends React.Component {
+  public render () {
+    const counter = this.props.counter;
+    return <div>{counter.value}</div>;
+  }
+})
+```
+
+You can use `Providers` to register multiple models without nested them as hell.
+
+```typescript
+import { Providers } from '@necolo/react-state';
+
+function App () {
+  return <Providers models={[
+    { model: CounterModel },
+    { model: SomeOtherModel, args: {}, memo: true }, // with more configs
+  ]}>
+    <SubPage />
+  </Providers>
+}
+
+// it's same as
+function App() {
+  return <Provider model={CounterModel}>
+    <Provider model={SomeOtherModel} args={{}} memo>
+      <SubPage />
+    </Provider>
+  </Provider>
+}
+```
---
+++ src/index.tsx
@@ -0,0 +1,200 @@
+import React, {
+  useEffect,
+  useContext,
+  useState,
+  useRef,
+  useDebugValue,
+  PropsWithChildren,
+  ReactElement,
+} from "react";
+
+interface Model<P extends any[] = any[], V = any> {
+  (...args: P): V;
+  context?: React.Context<Container<V>>;
+}
+type ModelP<S extends Model> = S extends Model<infer P, any> ? P : never;
+type ModelV<S extends Model> = S extends Model<any, infer V> ? V : never;
+
+interface ProviderProps<M extends Model> {
+  model: M;
+  args?: ModelP<M>;
+  memo?: boolean; // like React.memo, without memo it will always re-render
+  modelRef?: React.MutableRefObject<ModelV<M>>;
+}
+
+type Subscriber = () => void;
+class Container<V> {
+  private subscribers = new Set<Subscriber>();
+  constructor(public state?: V) {}
+
+  public notify() {
+    this.subscribers.forEach((sub) => sub());
+  }
+
+  public sub(sub: Subscriber) {
+    this.subscribers.add(sub);
+  }
+
+  public unsub(sub: Subscriber) {
+    this.subscribers.delete(sub);
+  }
+}
+
+const Processor = React.memo(
+  (props: {
+    onChange: (v: ModelV<Model>) => void;
+    model: Model;
+    args?: ModelP<Model>;
+    memo?: boolean;
+  }) => {
+    const args = props.args || [];
+    const result = props.model(...args);
+    useEffect(() => {
+      props.onChange(result);
+    });
+    return null;
+  },
+  (prevProps, nextProps) => {
+    if (!nextProps.memo) {
+      return false;
+    }
+    if (prevProps.args === nextProps.args) {
+      return true;
+    }
+    return isEquals(prevProps.args || [], nextProps.args || []);
+  }
+);
+
+function Provider<M extends Model, V extends ModelV<M>>(
+  props: PropsWithChildren<ProviderProps<M>>
+) {
+  const containerRef = useRef<Container<V>>();
+  if (!containerRef.current) {
+    containerRef.current = new Container();
+  }
+  const container = containerRef.current;
+
+  const Context = getModelContext(props.model, container);
+  const [isReady, setReady] = useState(false);
+
+  const onChange = (value: V) => {
+    if (!isReady) {
+      setReady(true);
+    }
+    container.state = value;
+    if (props.modelRef) {
+      props.modelRef.current = value;
+    }
+    container.notify();
+  };
+
+  return (
+    <Context.Provider value={container}>
+      <Processor
+        onChange={onChange}
+        model={props.model}
+        args={props.args}
+        memo={props.memo}
+      />
+      {isReady && props.children}
+    </Context.Provider>
+  );
+}
+
+function getModelContext<M extends Model, V extends ModelV<M>>(
+  model: Model,
+  container?: Container<V>
+) {
+  if (model.context) {
+    return model.context;
+  }
+  const context = React.createContext<Container<V>>(
+    container || new Container()
+  );
+  model.context = context;
+  return context;
+}
+
+type Deps<T> = (v: T) => unknown[];
+function useModel<M extends Model, V extends ModelV<M>>(
+  model: M,
+  deps?: Deps<V>
+) {
+  useDebugValue(model.name);
+  const container = useContext(getModelContext(model)) as Container<V>;
+  const [state, setState] = useState(container.state);
+  const depsRef = useRef<unknown[]>([]);
+
+  useEffect(() => {
+    const subscriber = () => {
+      if (!deps) {
+        setState(container.state);
+      } else {
+        const oldDeps = depsRef.current;
+        const newDeps = deps(container.state as V);
+        if (newDeps.length && !isEquals(oldDeps, newDeps)) {
+          setState(container.state);
+        }
+        depsRef.current = newDeps;
+      }
+    };
+    container.sub(subscriber);
+    return () => {
+      container.unsub(subscriber);
+    };
+  }, []);
+
+  if (!state) {
+    throw new Error(`The model ${model.name} is not initialized.`);
+  }
+
+  return state;
+}
+
+function isEquals(v1: unknown[], v2: unknown[]) {
+  if (v1.length !== v2.length) {
+    return false;
+  }
+  return v1.every((v, i) => v === v2[i]);
+}
+
+function Providers(props: {
+  models: ProviderProps<Model<any[]>>[];
+  children: React.ReactNode;
+}) {
+  return props.models.reverse().reduce((children, providerProps) => {
+    return <Provider {...providerProps}>{children}</Provider>;
+  }, props.children) as JSX.Element;
+}
+
+// for old classes use model
+function withModel(
+  models: { [name: string]: Model },
+  Component: React.ComponentType<any>
+) {
+  return function (props) {
+    const modelList = {};
+    for (let i = 0, keys = Object.keys(models); i < keys.length; i++) {
+      const name = keys[i];
+      // tslint:disable-next-line: react-hooks-nesting
+      const model = useModel(models[name]);
+      modelList[name] = model;
+    }
+    const _props = {
+      ...modelList,
+      ...props,
+    };
+    return <Component {..._props} />;
+  };
+}
+
+function Consumer<M extends Model>(props: {
+  model: M;
+  deps?: Deps<ModelV<M>>;
+  children: (model: ModelV<M>) => React.ReactNode;
+}) {
+  const model = useModel(props.model);
+  return props.children(model) as ReactElement;
+}
+
+export { Provider, Providers, useModel, Consumer, withModel };
